4b81224aac2fa69da0b0f63db7562d6ec810bd23
Chris Wiegman <cwiegman@me.com>
2014-07-19 20:04:21 -0500

    Remove custom header

--- functions.php
+++ functions.php
@@ -101,11 +101,6 @@ function chriswiegman_scripts() {
 add_action( 'wp_enqueue_scripts', 'chriswiegman_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
