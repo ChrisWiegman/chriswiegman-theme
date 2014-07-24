70b9e87a656b4eac71a1d0d7d505c04f7c389a45
Chris Wiegman <cwiegman@me.com>
2014-07-24 17:06:40 -0500

    Code cleanup

--- content.php
+++ content.php
@@ -9,20 +9,22 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php chriswiegman_posted_on(); ?>
-		</div><!-- .entry-meta -->
+			<div class="entry-meta">
+				<?php chriswiegman_posted_on(); ?>
+			</div><!-- .entry-meta -->
 		<?php endif; ?>
-	</header><!-- .entry-header -->
+	</header>
+	<!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'chriswiegman' ) ); ?>
 		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'chriswiegman' ),
-				'after'  => '</div>',
-			) );
+		wp_link_pages( array(
+			               'before' => '<div class="page-links">' . __( 'Pages:', 'chriswiegman' ),
+			               'after'  => '</div>',
+		               ) );
 		?>
-	</div><!-- .entry-content -->
+	</div>
+	<!-- .entry-content -->
 
 </article><!-- #post-## -->
