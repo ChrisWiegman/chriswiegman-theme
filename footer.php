f1fc5cdc8e4cc00a9e9ac91b5112135c9f66d2a1
Chris Wiegman <cwiegman@me.com>
2014-07-24 18:14:53 -0500

    Clean up the footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<span class="copyright">&copy; <?php echo date( 'Y', time() ); ?> Chris Wiegman - </span><span class="license"><a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">Creative Commons Licensed</a>.</span>
+			<p><span class="copyright">&copy; <?php echo date( 'Y', time() ); ?> Chris Wiegman - </span><span class="license"><a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">Creative Commons Licensed</a>.</span></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
