24e55d65f17af774ec4665ab1b45e31f7ab3d131
Chris Wiegman <cwiegman@me.com>
2014-07-24 18:48:05 -0500

    More header cleanup

--- header.php
+++ header.php
@@ -23,17 +23,18 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'chriswiegman' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
-			                          rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="wrap">
 
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<div class="site-branding">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			</div>
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Primary Menu', 'chriswiegman' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav>
+
 		</div>
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'chriswiegman' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav>
 		<!-- #site-navigation -->
 	</header>
 	<!-- #masthead -->
