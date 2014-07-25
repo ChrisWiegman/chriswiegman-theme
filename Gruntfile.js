9126046b2c50e85a54f1f09561ed7351e40acb13
Chris Wiegman <cwiegman@me.com>
2014-07-24 19:43:04 -0500

    Update grunt file for backstretch.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,8 @@ module.exports = function ( grunt ) {
 				files: {
 					'js/footer.min.js': [
 						'lib/js/navigation.js',
-					    'lib/js/skip-link-focus-fix.js',
+						'lib/js/skip-link-focus-fix.js',
+						'lib/js/backstretch.js',
 					    'lib/js/scripts.js'
 					]
 				}
