ecd1431b5f273e6fb64a4c615d67a293dd3f2e78
Chris Wiegman <cwiegman@me.com>
2014-07-24 19:43:37 -0500

    Change menu title

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 			</div>
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Primary Menu', 'chriswiegman' ); ?></button>
+				<button class="menu-toggle"><?php _e( 'Menu', 'chriswiegman' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 			</nav>
 
