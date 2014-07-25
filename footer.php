9c3e164f79a5e3a5b95dd89b73cb5e4d0a1dc22f
Chris Wiegman <cwiegman@me.com>
2014-07-24 20:01:04 -0500

    No period in footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<p><span class="copyright">&copy; <?php echo date( 'Y', time() ); ?> Chris Wiegman - </span><span class="license"><a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">Creative Commons Licensed</a>.</span></p>
+			<p><span class="copyright">&copy; <?php echo date( 'Y', time() ); ?> Chris Wiegman - </span><span class="license"><a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">Creative Commons Licensed</a></span></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
