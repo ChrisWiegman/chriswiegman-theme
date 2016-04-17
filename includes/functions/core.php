87aae19afb7fec04fc68461141f61811a2d644fc
Chris Wiegman <chris@chriswiegman.com>
2016-04-17 10:01:16 -0400

    Fix jQuery version

--- includes/functions/core.php
+++ includes/functions/core.php
@@ -256,7 +256,7 @@ function filter_wp_default_scripts( $scripts ) {
 	if ( ! is_admin() ) {
 
 		$scripts->remove( 'jquery' );
-		$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.11.3' );
+		$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.3' );
 
 	}
 
