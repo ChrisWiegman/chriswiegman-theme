7d4926815903e4e72fa1da8eb0a3c8772d1f62d7
Chris Wiegman <chris@wiegman.us>
2016-10-30 18:42:21 -0400

    A marginally better 404 page

--- 404.php
+++ 404.php
@@ -15,52 +15,22 @@ use CW\Theme\Functions\Template_Tags;
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'chriswiegman' ); ?></h1>
-			</header><!-- .page-header -->
+			<section class="no-results not-found hentry">
+				<header class="page-header">
+					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'chriswiegman' ); ?></h1>
+				</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'chriswiegman' ); ?></p>
+				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'chriswiegman' ); ?></p>
 
 				<?php get_search_form(); ?>
 
-				<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+			</section>
 
-				<?php if ( Template_Tags\categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'chriswiegman' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-				<?php endif; ?>
+		</main><!-- #main -->
+	</section><!-- #primary -->
 
-				<?php
-				/* translators: %1$s: smiley */
-				$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'chriswiegman' ), convert_smilies( ':)' ) ) . '</p>';
-				the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-				?>
-
-				<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-</div><!-- #primary -->
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
