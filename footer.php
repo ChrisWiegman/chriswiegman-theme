58a8fc9ef8306afb7e8913a659f73b013a2ef2a4
Chris Wiegman <chris@wiegman.us>
2016-11-11 19:38:22 -0500

    Remove simplified GA code

--- footer.php
+++ footer.php
@@ -33,24 +33,5 @@ namespace CW\Theme\Templates\Footer;
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<?php if ( ! is_user_logged_in() ) { ?>
-	<script>
-		(function ( i, s, o, g, r, a, m ) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-					(i[r].q = i[r].q || []).push( arguments )
-				}, i[r].l = 1 * new Date();
-			a = s.createElement( o ),
-				m = s.getElementsByTagName( o )[0];
-			a.async = 1;
-			a.src   = g;
-			m.parentNode.insertBefore( a, m )
-		})( window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga' );
-
-		ga( 'create', 'UA-85532749-1', 'auto' );
-		ga( 'send', 'pageview' );
-
-	</script>
-<?php } ?>
 </body>
 </html>
