9fca6ad1b6f6d86dbc90d52f7ec3e2c1876a9d64
Chris Wiegman <chris@wiegman.us>
2017-01-24 19:55:14 -0500

    Fix page header

--- page-about.php
+++ page-about.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * The template for the speaking page
+ * The template for the about page
  *
  * @since   5.0.0
  *
- * @package CW\Theme\Templates\Page\Speaking
+ * @package CW\Theme\Templates\Page\About
  *
  * @author  Chris Wiegman <chris@chriswiegman.com>
  */
 
-namespace CW\Theme\Templates\Page\Speaking;
+namespace CW\Theme\Templates\Page\About;
 
 get_header(); ?>
 
