4110adacf94d657c644f6ee46d1c77ac612cb82c
Chris Wiegman <chris@wiegman.us>
2017-11-03 16:39:32 -0400

    version bump

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 use CW\Theme\Post_Types;
 
 // Useful global constants.
-define( 'CW_THEME_VERSION', '7.0' );
+define( 'CW_THEME_VERSION', '7.0.1' );
 define( 'CW_THEME_PATH', get_template_directory() . '/' );
 define( 'CW_THEME_URL', get_stylesheet_directory_uri() );
 define( 'CW_THEME_INCLUDES', CW_THEME_PATH . 'includes/' );
