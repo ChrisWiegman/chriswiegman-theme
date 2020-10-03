7dc7824314d6494c851597762ca20fbf69146316
Chris Wiegman <chris@chriswiegman.com>
2020-10-03 18:45:52 -0400

    At least let default tests run

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,4 +11,4 @@ WP_Mock::bootstrap();
  * Now we include any plugin files that we need to be able to run the tests. This
  * should be files that define the functions and classes you're going to test.
  */
-require_once dirname( dirname( __FILE__ ) )  . '/theme/functions.php';
+#require_once dirname( dirname( __FILE__ ) )  . '/theme/functions.php';
