a29479d4154cb427d3adf74497ce3d88797cbf1a
Chris Wiegman <chris@chriswiegman.com>
2021-12-20 15:44:00 -0500

    Move theme to root and update

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
+      globalReturn: true,
+    },
+    sourceType: "module",
+  },
+  extends: ["plugin:react/recommended", "plugin:prettier/recommended"],
+  plugins: [],
+  // add your custom rules here
+  ignorePatterns: ["**/dist", "**/includes/settings/scss/bootstrap"],
+  rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "react/prop-types": 0,
+    "react/no-unescaped-entities": 0,
+    "react/no-children-prop": 0,
+    "no-unsafe-optional-chaining": 0,
+  },
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,50 @@
-var gulp = require('gulp');
-var gulpSass = require('gulp-sass')(require('sass'));
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
+var gulp = require("gulp");
+var gulpSass = require("gulp-sass")(require("sass"));
+var rename = require("gulp-rename");
+var sourcemaps = require("gulp-sourcemaps");
+const del = require("del");
 
-function clean () {
-    return del(["./theme/assets/*.css"]);
+function clean() {
+  return del(["./assets/*.css"]);
 }
 
+function sass() {
+  gulpSass.compiler = require("node-sass");
 
-function sass () {
-
-    gulpSass.compiler = require('node-sass');
-
-    return gulp.src('./theme/assets/scss/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(gulpSass().on('error', gulpSass.logError))
-        .pipe(rename('main.css'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./theme/assets/'));
-
+  return gulp
+    .src("./assets/scss/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(gulpSass().on("error", gulpSass.logError))
+    .pipe(rename("main.css"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./assets/"));
 }
 
-function editorSass () {
+function editorSass() {
+  gulpSass.compiler = require("node-sass");
 
-    gulpSass.compiler = require('node-sass');
-
-    return gulp.src('./theme/assets/scss/editor.scss')
-        .pipe(gulpSass().on('error', gulpSass.logError))
-        .pipe(rename('editor.css'))
-        .pipe(gulp.dest('./theme/assets/'));
-
+  return gulp
+    .src("./assets/scss/editor.scss")
+    .pipe(gulpSass().on("error", gulpSass.logError))
+    .pipe(rename("editor.css"))
+    .pipe(gulp.dest("./assets/"));
 }
 
-function sassMin () {
+function sassMin() {
+  gulpSass.compiler = require("node-sass");
 
-    gulpSass.compiler = require('node-sass');
-
-    return gulp.src('./theme/assets/scss/main.scss')
-        .pipe(gulpSass({ outputStyle: 'compressed' }).on('error', gulpSass.logError))
-        .pipe(rename('main.min.css'))
-        .pipe(gulp.dest('./theme/assets/'));
-
+  return gulp
+    .src("./assets/scss/main.scss")
+    .pipe(
+      gulpSass({ outputStyle: "compressed" }).on("error", gulpSass.logError)
+    )
+    .pipe(rename("main.min.css"))
+    .pipe(gulp.dest("./assets/"));
 }
 
 // Watch files
-function watchFiles () {
-    gulp.watch("./theme/assets/scss/**/*", sass, sassMin, editorSass);
+function watchFiles() {
+  gulp.watch("./assets/scss/**/*", sass, sassMin, editorSass);
 }
 
 const build = gulp.series(clean, sass, sassMin, editorSass);
