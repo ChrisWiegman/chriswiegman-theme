69f47fbec69db090380c665df2eedb2709f19ecf
Chris Wiegman <chris@chriswiegman.com>
2022-02-27 10:27:21 -0500

    Fix REST issues in wp-admin

--- functions.php
+++ functions.php
@@ -33,7 +33,6 @@ function init() {
 	add_action( 'pre_get_posts', $n( 'action_pre_get_posts' ) );
 	add_action( 'admin_init', $n( 'action_admin_init' ) );
 	add_filter( 'xmlrpc_enabled', '__return_false' );
-	add_filter( 'rest_enabled', '__return_false' );
 
 	// Close comments on the front-end.
 	add_filter( 'comments_open', '__return_false', 20, 2 );
@@ -55,20 +54,6 @@ function init() {
 		remove_action( 'wp_head', 'rest_output_link_wp_head' );
 		remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
 		remove_action( 'wp_head', 'rest_output_link_wp_head', 10, 0 );
-		remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
-		remove_action( 'auth_cookie_malformed', 'rest_cookie_collect_status' );
-		remove_action( 'auth_cookie_expired', 'rest_cookie_collect_status' );
-		remove_action( 'auth_cookie_bad_username', 'rest_cookie_collect_status' );
-		remove_action( 'auth_cookie_bad_hash', 'rest_cookie_collect_status' );
-		remove_action( 'auth_cookie_valid', 'rest_cookie_collect_status' );
-		remove_filter( 'rest_authentication_errors', 'rest_cookie_check_errors', 100 );
-		remove_action( 'init', 'rest_api_init' );
-		remove_action( 'rest_api_init', 'rest_api_default_filters', 10, 1 );
-		remove_action( 'parse_request', 'rest_api_loaded' );
-		remove_action( 'rest_api_init', 'wp_oembed_register_route' );
-		remove_filter( 'rest_pre_serve_request', '_oembed_rest_pre_serve_request', 10, 4 );
-		remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
-		remove_action( 'wp_head', 'wp_oembed_add_host_js' );
 
 	}
 }
