2690c4d28fbe12ee2b06326084cb1510e6bea1f4
Chris Wiegman <chris@chriswiegman.com>
2022-04-04 13:58:52 -0400

    Remove extra favicon tag

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="icon" href="/favicon.ico" type="image/x-icon">
 	<?php wp_head(); ?>
 </head>
 
