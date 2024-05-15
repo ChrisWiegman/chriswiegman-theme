d4e90552c81efab08b25c4bb08ec8e4ee16e1484
Chris Wiegman <chris@chriswiegman.com>
2024-05-15 12:25:04 -0500

    Working on basic homepage tests

---
+++ tests/e2e/specs/homepage.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has theme title', async ({ page }) => {
+  await page.goto('https://localhost:8889/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/chriswiegman-theme/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://localhost:8889/');
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Recent Posts' })).toBeVisible();
+});
