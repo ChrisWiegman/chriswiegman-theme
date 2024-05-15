a56800c3287fc0b544337b3fc997f51897baa2b3
Chris Wiegman <chris@chriswiegman.com>
2024-05-15 12:33:47 -0500

    Use the correct site

--- tests/e2e/specs/homepage.spec.js
+++ tests/e2e/specs/homepage.spec.js
@@ -2,14 +2,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('has theme title', async ({ page }) => {
-  await page.goto('http://localhost:8889/');
+  await page.goto('http://localhost:8888/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/chriswiegman-theme/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('http://localhost:8889/');
+test('has page title', async ({ page }) => {
+  await page.goto('http://localhost:8888/');
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Recent Posts' })).toBeVisible();
